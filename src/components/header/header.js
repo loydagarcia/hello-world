2f26859ae1223e4b56aff8bb7796dad50a0f0457
Gustavo <kyg7887@gmail.com>
2018-12-11 17:35:11 -0600

    Initial commit, adding some components

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        Este es mi header
+      </div>
+    );
+  }
+}
+
+export default Header;
